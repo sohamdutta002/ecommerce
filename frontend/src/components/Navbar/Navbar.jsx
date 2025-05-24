3b78d1a020a985946b79d87ab27b8179e6947b64
Soham <duttasoham2222@gmail.com>
2025-05-24 23:48:39 +0530

    logout button added

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -12,12 +12,11 @@ const Navbar = () => {
     }
     return (
         <div>
-            Navbar
             {user && (
-                <>
+                <div className='bg-purple-400 flex justify-between items-center p-2'>
                     <span>{user.name}</span>
-                    <button onClick={handleLogout}>Logout</button>
-                </>
+                    <button onClick={handleLogout} className='p-2 bg-blue-500 rounded-md hover:bg-blue-400 hover:cursor-pointer'>Logout</button>
+                </div>
             )}
         </div>
     )
